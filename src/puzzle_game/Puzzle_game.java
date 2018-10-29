d84058bef9bf5fce45ab9b22b8043b2737a747c9
Farouk Aouni <aouni.farouk@gmail.com>
2018-10-29 18:41:19 +0100

    First commit

---
+++ src/puzzle_game/Puzzle_game.java
@@ -0,0 +1,30 @@
+/*
+ * Java Utveckling 18
+ */
+
+package puzzle_game;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * 
+ * @author Farouk A
+ */
+public class Puzzle_game extends JFrame{
+
+      JPanel jPanel = new JPanel();
+      JPanel jPanel1= new JPanel();
+
+      JButton buttons[]= new JButton[15];
+      JLabel jLabel=new JLabel();
+      JButton newGame=new JButton("New Game");
+    
+   
+    public static void main(String[] args) {
+        
+    }
+
+}
